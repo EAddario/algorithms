b6e6d82a4d0f9d61acfa2d81ddba58ec88d91075
Ed Addario <eaddario@hotmail.com>
2021-02-13 16:24:18 +0000

    add binarySearch

---
+++ cmd/binarySearch/main.go
@@ -0,0 +1,49 @@
+/******************************************************************************
+ *  Execution:    go run cmd/binarySearch/main.go tinyAllowlist.txt < tinyText.txt
+ *  50
+ *  99
+ *
+ *  % binarySearch largeAllowlist.txt < largeText.txt | more
+ *  499569
+ *  984875
+ *  295754
+ *  207807
+ *  140925
+ *  161828
+ *  [367,966 total values]
+ *
+ ******************************************************************************/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+
+	"../../algorithms"
+	"../../stdin"
+)
+
+func main() {
+
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: binarySearch file < file")
+		os.Exit(0)
+	}
+
+	arr := stdin.NewIn(os.Args[1], bufio.ScanWords).ReadAllInts()
+	sort.Ints(arr)
+	si := stdin.NewStdIn(bufio.ScanLines)
+
+	for !si.IsEmpty() {
+		key := si.ReadInt()
+
+		if algorithms.IndexOf(&arr, key) == -1 {
+			fmt.Println(key)
+		}
+
+	}
+
+}
